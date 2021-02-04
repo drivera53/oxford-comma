f7f0f5602e405fa5ad62a5bb9f175780b95943c0
Danny Rivera <samboard@hotmail.com>
2021-02-04 07:45:45 -0500

    commit tree c82159ab19620edfd7d19b243d10b78e75f21b33

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size <= 2 
+        new_array = array.join(" and ")
+        return new_array
+    else
+        last = array.pop
+        and_last = "and " + last
+        array << and_last
+        new_array = array.join(", ")
+        return new_array
+    end
 end
+    
